f66c9620ea2635a62e87f9e1301f5e1c276931d9
walid younis <walid.younis.90@gmail.com>
2020-04-05 05:20:16 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,3 +24,14 @@ def output_array(array)
     count += 1
   end
 end
+
+
+def return_string_array(array)
+  count = 0
+  new_array = []
+  while count < array.length do
+    new_array.push(array[count].to_s)
+    count += 1
+  end
+  new_array
+end
