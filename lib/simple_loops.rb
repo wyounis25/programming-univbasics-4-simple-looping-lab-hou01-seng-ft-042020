6cd7a72800455d60f9252e1722a6386e17a978ef
walid younis <walid.younis.90@gmail.com>
2020-04-05 05:14:52 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,3 +7,11 @@ def loop_message_five_times(array)
     counter += 1
   end
 end
+
+def loop_message_n_times(message, limit = 5)
+  count = 0
+  while count < limit do
+    puts message
+    count += 1
+  end
+end
