0f214c82c5b898ffe1ab14f3152fd0d605bc0f61
walid younis <walid.younis.90@gmail.com>
2020-04-05 05:12:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,23 @@
 # Write your methods here
+
+def loop_message_five_times(array)
+  counter = 0
+  while counter < 6 do
+    puts array[counter]
+    counter + = 1
+  end
+end
+
+
+def loop_message_n_times(array, whatt = 6)
+  counter = 0
+  while counter < whatt do
+    puts array[counter]
+  end
+end
+
+def output_array
+end
+
+def return_string_array
+end
